d3830ca64cb15d1827b4394a25cbf45991c9a1a2
Suzanne van Dam <suzannevandam@gmailcom>
2015-11-03 12:31:10 +0000

    Second commit. Added the python code that computes the answer.

---
+++ qanda.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def compute_answer(q):
+	a=42
+	print('your question was',q)
+	print('the answer is',a)
+	return(42)
+
+if __name__ == '__main__':
+	compute_answer('type your question here')
